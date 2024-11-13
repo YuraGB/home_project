ef8ba4185472e42cf24c457a0ed68753204d214e
yuhur <yurii.h@fisha.co.il>
2024-11-13 11:47:20 +0200

    init, auth, i18n

---
+++ home_project/next-auth.d.ts
@@ -0,0 +1,31 @@
+// Ref: https://next-auth.js.org/getting-started/typescript#module-augmentation
+
+import { DefaultSession, DefaultUser } from 'next-auth'
+import { DefaultJWT } from 'next-auth/jwt'
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string
+      role: string
+    } & DefaultSession
+  }
+
+  interface User extends DefaultUser {
+    role: string
+    id: string
+  }
+}
+
+declare module '@auth/core/adapters' {
+  interface AdapterUser {
+    role: string
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT extends DefaultJWT {
+    role: string
+    id: string
+  }
+}
