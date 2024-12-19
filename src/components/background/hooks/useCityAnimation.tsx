f4e84af15633009a6ea4c4d35579ce09351b9100
yuhur <yurii.h@fisha.co.il>
2024-12-19 16:03:45 +0200

    feat: change model city

--- src/components/background/hooks/useCityAnimation.tsx
+++ src/components/background/hooks/useCityAnimation.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useRef } from "react";
 import { CameraControls } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { usePathname } from "next/navigation";
 import * as THREE from "three";
 import { useMotionValue, useSpring } from "framer-motion";
+import { Group } from "three";
 
 const { DEG2RAD } = THREE.MathUtils;
 
 export const useCityAnimation = () => {
   const cameraControlRef = useRef<CameraControls | null>(null);
+  const cityRef = useRef<Group>(null);
   const { camera } = useThree();
   const path = usePathname();
   const wasAnimated = useRef(false); // animation should be only ones
@@ -20,6 +22,12 @@ export const useCityAnimation = () => {
     stiffness: 20,
   });
 
+  useFrame((state, delta) => {
+    if (cityRef.current) {
+      cityRef.current.rotation.y += delta * 0.01; //  all time rotation
+    }
+  });
+
   useEffect(() => {
     if (
       cameraControlRef.current &&
@@ -29,14 +37,14 @@ export const useCityAnimation = () => {
     ) {
       wasAnimated.current = true;
 
-      //animate the camera
+      //animate the camera to the city on the start
       cameraControlRef.current.rotate(-75 * DEG2RAD, 0, true);
-      cameraControlRef.current.rotate(0, 2 * DEG2RAD, true);
-      cameraControlRef.current.zoom(-camera.zoom / 2, true);
+      cameraControlRef.current.rotate(0, 4.3 * DEG2RAD, true);
+      cameraControlRef.current.zoom(camera.zoom / 2, true);
       cameraControlRef.current.truck(0, 37, true);
 
       // make the city up
-      yPositionStart.set(-53);
+      yPositionStart.set(-50);
     }
   }, [camera.zoom, path, yPositionStart]);
 
@@ -49,5 +57,6 @@ export const useCityAnimation = () => {
   return {
     cameraControlRef,
     position,
+    cityRef,
   };
 };
