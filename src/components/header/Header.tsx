0bca7e198ad1ba4cde7ac5c077e773d3174ddee8
yuhur <yurii.h@fisha.co.il>
2025-01-04 16:47:55 +0200

    feat: resolutions

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,22 @@
 import { ModeToggle } from "@/components/theme/themeSwitcher";
 import { Profile } from "@/components/user/Profile";
+import { Logo } from "@/components/header/Logo";
 
 export const Header = () => {
   return (
     <header
       className={
-        "grid grid-cols-[1fr_minmax(320px,_900px)_1fr] items-center border-b-2 p-2 mb-2 absolute w-full top-0 backdrop-blur left-0"
+        "grid grid-cols-[1fr_minmax(320px,_900px)_1fr] items-center border-b-2 p-2 mb-2 w-full top-0 backdrop-blur left-0"
       }
     >
       <section
-        className={"col-start-2 grid grid-cols-3 justify-items-center w-full"}
+        className={"col-start-2 grid grid-cols-2 justify-items-center w-full"}
       >
-        <ModeToggle />
-        <p>Logo</p>
-        <Profile />
+        <Logo />
+        <section className={"flex items-center gap-4"}>
+          <ModeToggle />
+          <Profile />
+        </section>
       </section>
     </header>
   );
--- src/components/pageLayout/defaultPageLayout.tsx
+++ src/components/pageLayout/defaultPageLayout.tsx
@@ -6,7 +6,7 @@ export const DefaultPageLayout = ({
   children: ReactNode;
 }): ReactNode => {
   return (
-    <div className="grid  min-h-screen py-20 px-8 sm:p-20 font-[family-name:var(--font-geist-sans)] md:px-8 md:py-20">
+    <div className="grid py-10 px-8 sm:p-10 font-[family-name:var(--font-geist-sans)] md:px-8 md:py-10">
       <main className="flex flex-col h-full items-center sm:items-start w-full max-w-7xl m-auto overflow-hidden">
         {children}
       </main>
