d8286f2d850e9a546e1a4df5d7e36222a0ac0cc0
yuhur <yurii.h@fisha.co.il>
2025-01-11 17:03:47 +0200

    fix: hide heavy elements

--- src/components/background/Model.tsx
+++ src/components/background/Model.tsx
@@ -1,36 +1,14 @@
-import { Sky } from "@react-three/drei";
-
 import dynamic from "next/dynamic";
+import React from "react";
 
 const City2 = dynamic(() =>
   import("@/components/background/City2").then((mod) => mod.City2),
-);
-const Wall = dynamic(() =>
-  import("@/components/background/Wall").then((mod) => mod.Wall),
 );
 
 export const Model = () => {
   return (
     <>
-      <directionalLight
-        position={[-100, 90, 550]}
-        intensity={0.8}
-        castShadow={true}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-radius={1200}
-      >
-        <orthographicCamera
-          attach="shadow-camera"
-          args={[-100, 300, 300, -100]}
-        />
-      </directionalLight>
-      <ambientLight intensity={0.8} />
-
-      <Wall />
-      {/*<City />*/}
       <City2 />
-      <Sky distance={10000} rayleigh={0.5} turbidity={5} />
     </>
   );
 };
--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import dynamic from "next/dynamic";
 import { Ocean } from "@/components/background/Ocean";
+import { Sky } from "@react-three/drei";
 
 const Model = dynamic(() =>
   import("@/components/background/Model").then((mod) => mod.Model),
@@ -12,14 +13,30 @@ export const Background = (): ReactNode => {
   return (
     <article className={"z-[-1] min-h-[700px] fixed w-full h-dvh top-0 left-0"}>
       <Canvas
+        performance={{ min: 0.5 }}
         camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 10, 300] }}
         shadows={true}
         className={"z-[-1]"}
       >
+        <directionalLight
+          position={[-100, 90, 550]}
+          intensity={0.8}
+          castShadow={true}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-radius={1200}
+        >
+          <orthographicCamera
+            attach="shadow-camera"
+            args={[-100, 300, 300, -100]}
+          />
+        </directionalLight>
+        <ambientLight intensity={0.8} />
         <Suspense fallback={null}>
           <Ocean />
           <Model />
         </Suspense>
+        <Sky distance={10000} rayleigh={0.5} turbidity={5} />
         <fog attach="fog" args={["#aabbcc", 200, 400]} />
       </Canvas>
     </article>
