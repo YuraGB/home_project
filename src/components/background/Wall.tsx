47834b7c8ee0bec3fda049acac8ff8e73873d308
yuhur <yurii.h@fisha.co.il>
2025-01-11 17:06:45 +0200

    fix: hide heavy elements

--- src/components/background/Wall.tsx
+++ src/components/background/Wall.tsx
@@ -3,6 +3,14 @@ import { TextContent } from "@/components/background/TextContent";
 import { useWall } from "@/components/background/hooks/useWall";
 import { motion } from "framer-motion-3d";
 
+/**
+ * This is heavy component
+ * possible because the Loaders -> textures/fonts
+ * was removed but don't deleted.
+ * Possible to resolve the issue in the future
+ * @constructor
+ */
+
 export const Wall: React.FC = () => {
   const { material, position, rotation, size } = useWall();
   return (
