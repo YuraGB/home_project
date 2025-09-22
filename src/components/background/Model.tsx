85e5d01453aedfb89b4267c3e2fcc1789267d3db
Yuhur <yuhur1985@gmail.com>
2025-09-22 16:58:20 +0300

    fix: global error bg 3d fix

--- src/components/background/Model.tsx
+++ src/components/background/Model.tsx
@@ -1,8 +1,9 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
-const City2 = dynamic(() =>
-  import("@/components/background/City2").then((mod) => mod.City2),
+const City2 = dynamic(
+  () => import("@/components/background/City2").then((mod) => mod.City2),
+  { ssr: false },
 );
 
 export const Model = () => {
