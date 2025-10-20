8c3da2cfb6ad455c07531d85e9e29ee80489306c
Yuhur <yuhur1985@gmail.com>
2025-10-20 14:53:12 +0300

    fix: log error during install, viewTransition error

--- src/components/pageTitle/index.tsx
+++ src/components/pageTitle/index.tsx
@@ -1,20 +1,18 @@
+"use client";
 import { ReactNode } from "react";
-import { unstable_ViewTransition as ViewTransition } from "react";
 type PageTitleProps = {
   title: string;
   transitionname?: string;
 };
 
-export const PageTitle = ({ title, ...rest }: PageTitleProps): ReactNode => {
+export const PageTitle = ({ title }: PageTitleProps): ReactNode => {
   return (
     <section
       className={
         "flex items-center justify-center p-4 text-2xl w-full border-b"
       }
     >
-      <ViewTransition name={rest.transitionname || ""}>
-        <h1 className="nline-block font-bold text-gray-700">{title}</h1>
-      </ViewTransition>
+      <h1 className="nline-block font-bold text-gray-700">{title}</h1>
     </section>
   );
 };
